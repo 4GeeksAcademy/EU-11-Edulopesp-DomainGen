01271aade80068dbf2200622b38a6e1c7d8370b5
Edulopesp <129979638+Edulopesp@users.noreply.github.com>
2023-05-22 19:02:25 +0000

    the domain gen

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#phrase").innerHTML = generateDomain();
+};
+//write your code here
+
+const generateDomain = () => {
+  const who = [
+    "eminems",
+    "chewbaccas",
+    "thedudefromapple",
+    "mygirlfriends",
+    "yourdoctors"
+  ];
+  const what = ["dream", "nightmare", "goal"];
+  const where = ["website", "project", "place", "portifolio"];
+
+  const getRandomDomain = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  return `${getRandomDomain(who)}${getRandomDomain(what)}${getRandomDomain(
+    where
+  )}.com`;
 };
