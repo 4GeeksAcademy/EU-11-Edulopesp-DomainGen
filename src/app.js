1ff71d681afb3c8b2b42b64c6daa8317f1e74546
Edulopesp <129979638+Edulopesp@users.noreply.github.com>
2023-05-30 16:25:28 +0000

    remake of the gen domain with 10 options

--- src/app.js
+++ src/app.js
@@ -3,24 +3,28 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  document.querySelector("#phrase").innerHTML = generateDomain();
-};
-//write your code here
+  const phrases = document.querySelectorAll(".phrase");
 
+  phrases.forEach(phrase => {
+    phrase.innerHTML = generateDomain();
+  });
+};
+const genRandomDomain = arr => arr[Math.floor(Math.random() * arr.length)];
+//write your code here
 const generateDomain = () => {
-  const who = [
+  const owner = [
     "eminems",
     "chewbaccas",
     "thedudefromapple",
     "mygirlfriends",
     "yourdoctors"
   ];
-  const what = ["dream", "nightmare", "goal"];
+
+  const place = ["dream", "nightmare", "goal"];
   const where = ["website", "project", "place", "portifolio"];
+  const extension = [".com", ".us", ".net", ".io", ".pt", ".com.br"];
 
-  const getRandomDomain = arr => arr[Math.floor(Math.random() * arr.length)];
-
-  return `${getRandomDomain(who)}${getRandomDomain(what)}${getRandomDomain(
-    where
-  )}.com`;
+  return `www.${genRandomDomain(owner)}${genRandomDomain(
+    place
+  )}${genRandomDomain(where)}${genRandomDomain(extension)}`;
 };
