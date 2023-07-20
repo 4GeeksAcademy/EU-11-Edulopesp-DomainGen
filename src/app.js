ab605ad03d8baa3ef89f24684436cdc6ce08d905
Eduardo Lopes <129979638+Edulopesp@users.noreply.github.com>
2023-07-20 22:58:12 +0000

    now its right

--- src/app.js
+++ src/app.js
@@ -1,26 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const firstPhrase = ["chewbacca", "eminem", "theguyfromapple", "yourdoctor"];
+const secoundPhrase = ["dream", "nightmare", "portifolio", "house"];
+const extension = [".com", ".us", ".net", ".io", ".pt"];
 
-window.onload = function() {
-  const phrases = document.querySelectorAll(".phrase");
+const phrasesContainer = document.getElementById("phrasesContainer");
 
-  const genRandomDomain = arr => arr[Math.floor(Math.random() * arr.length)];
-  const shuffleArray = arr => arr.sort(() => Math.random() - 0.5);
+function generateDomain() {
+  const getRandomDomain = arr => arr[Math.floor(Math.random() * arr.length)];
 
-  const randomDomains = [
-    "whereeminemwritessongs",
-    "chewbaccasdreamplace",
-    "thedudefromappleportifolio",
-    "somewherethateverybodylikes",
-    "yourdoctorsplace"
-  ];
-  const extension = [".com", ".us", ".net", ".io", ".pt"];
-  const shuffledDomains = shuffleArray(randomDomains);
+  return `www.${getRandomDomain(firstPhrase)}${getRandomDomain(
+    secoundPhrase
+  )}${getRandomDomain(extension)}`;
+}
 
-  phrases.forEach((phrase, index) => {
-    phrase.innerHTML = `www.${shuffledDomains[index]}${genRandomDomain(
-      extension
-    )}`;
-  });
-};
+function generateDomains() {
+  phrasesContainer.innerHTML = "";
+  const totalCombinations =
+    firstPhrase.length * secoundPhrase.length * extension.length;
+
+  for (let i = 0; i < totalCombinations; i++) {
+    const domain = generateDomain();
+    const paragraph = document.createElement("p");
+    paragraph.textContent = domain;
+    phrasesContainer.appendChild(paragraph);
+  }
+}
+
+generateDomains();
+
+//preciso treinar interpretação de texto né kkkkk 3 tentativas pra entender oq era pra fazer e desculpa a demora, tive que pesquisar sobre o for loop, tava meio perdido doq eu podia fazer.
